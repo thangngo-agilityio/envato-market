641a088d3d1de80d43b508198228e75eb947d924
Son Xuan Son <son.phan@asnet.com.vn>
2023-12-29 10:57:52 +0700

    feat: heading tag component

---
+++ landing-page/src/types/Heading.ts
@@ -0,0 +1,11 @@
+export enum TYPE_HEADING {
+  MAIN_TITLE = 'main_title',
+  SUB_TITLE = 'sub_title',
+  CARD_TITLE = 'card_title',
+}
+
+export enum TAG_HEADING {
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+}
--- landing-page/tailwind.config.mjs
+++ landing-page/tailwind.config.mjs
@@ -1,60 +1,61 @@
-import plugin from "tailwindcss/plugin";
+import plugin from 'tailwindcss/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       screen: {
-        xs: "320px",
+        xs: '320px',
         // => @media (min-width: 375px) { ... }
       },
       fontFamily: {
-        primary: ["Helvetica", "sans-serif"],
+        primary: ['Helvetica', 'sans-serif'],
       },
       lineHeight: {
-        xs: "13px",
-        sm: "18px",
-        base: "21px",
-        md: "23px",
-        lg: "26px",
-        xl: "30px",
-        huge: "31px",
-        giant: "47px",
+        xs: '13px',
+        sm: '18px',
+        base: '21px',
+        md: '23px',
+        lg: '26px',
+        xl: '30px',
+        huge: '31px',
+        big: '39px',
+        giant: '47px',
       },
       colors: {
-        primary: "#242424",
-        secondary: "#131212",
-        elementary: "#6b6b6b",
-        average: "#252525",
-        sparingly: "#6d6d6d",
-        little: "#b3b1b1",
-        once: "#212529",
-        onceAll: "#6c757d",
+        primary: '#242424',
+        secondary: '#131212',
+        elementary: '#6b6b6b',
+        average: '#252525',
+        sparingly: '#6d6d6d',
+        little: '#b3b1b1',
+        once: '#212529',
+        onceAll: '#6c757d',
       },
       backgroundColor: {
-        sun: "#fbb710",
-        iridium: "#3c3c3c",
-        desertStorm: "#f5f7fa",
-        few: "#3c3c3cb3",
+        sun: '#fbb710',
+        iridium: '#3c3c3c',
+        desertStorm: '#f5f7fa',
+        few: '#3c3c3cb3',
       },
       spacing: {
-        md: "15px",
-        lg: "30px",
-        xl: "50px",
-        "2xl": "60px",
-        "3xl": "100px",
-        "4xl": "386px",
+        md: '15px',
+        lg: '30px',
+        xl: '50px',
+        '2xl': '60px',
+        '3xl': '100px',
+        '4xl': '386px',
       },
     },
   },
   plugins: [
     plugin(function ({ addBase }) {
       addBase({
-        "@font-face": {
-          fontFamily: "Helvetica",
-          fontWeight: "400",
-          src: "url(/assets/fonts/Helvetica-Neue-Regular.woff2)",
-          fontDisplay: "swap",
+        '@font-face': {
+          fontFamily: 'Helvetica',
+          fontWeight: '400',
+          src: 'url(/assets/fonts/Helvetica-Neue-Regular.woff2)',
+          fontDisplay: 'swap',
         },
       });
     }),
