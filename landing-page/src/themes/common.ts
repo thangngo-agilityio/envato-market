1d0fb2b9b285fc2bf5c76ef59f7e51511f5b2f50
Son Xuan Son <son.phan@asnet.com.vn>
2024-01-02 10:13:33 +0700

    feat: update style code

--- landing-page/src/themes/common.ts
+++ landing-page/src/themes/common.ts
@@ -3,5 +3,6 @@ import type { Config } from 'tailwindcss';
 export const boxShadow: Config['theme'] = {
   boxShadow: {
     sm: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    navMobile: 'rgba(0, 0, 0, 0.15) 0px 4px 15px 0px',
   },
 };
