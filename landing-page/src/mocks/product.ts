e4dbe108c90cd1c63d7c25b74e4786f53d1ec629
duong.pham <duong.pham@asnet.com.vn>
2024-01-02 15:00:00 +0700

    feat: create ProductCard component

---
+++ landing-page/src/mocks/product.ts
@@ -0,0 +1,57 @@
+export const PRODUCTS = [
+  {
+    createdAt: '2024-01-01T17:24:10.516Z',
+    name: 'Ergonomic Steel Chair',
+    imageURLs: ['https://loremflickr.com/640/480/technics'],
+    description:
+      'Laboriosam nostrum voluptate voluptate itaque animi. Voluptatibus soluta mollitia ad. Aspernatur accusamus eos ullam eaque. Dolorem iste vel odio. Earum enim laudantium. Rerum eius molestias fuga dignissimos voluptatem.',
+    stock: 6632,
+    currency: '$',
+    amount: 2735,
+    id: '1',
+  },
+  {
+    createdAt: '2024-01-01T18:21:38.976Z',
+    name: 'Gorgeous Frozen Soap',
+    imageURLs: ['https://loremflickr.com/640/480/technics'],
+    description:
+      'Illum rem repellendus officiis unde. Totam magni totam dolor quam doloribus dicta deleniti nisi. Doloribus deleniti consequatur. Nostrum beatae nihil.',
+    stock: 65360,
+    currency: '$',
+    amount: 91995,
+    id: '2',
+  },
+  {
+    createdAt: '2024-01-01T18:40:49.933Z',
+    name: 'Tasty Bronze Mouse',
+    imageURLs: ['https://loremflickr.com/640/480/technics'],
+    description:
+      'Modi cum iste cumque. Eveniet occaecati rem temporibus incidunt atque qui. Hic repudiandae aspernatur sit.',
+    stock: 66827,
+    currency: '$',
+    amount: 96609,
+    id: '3',
+  },
+  {
+    createdAt: '2024-01-01T13:27:14.519Z',
+    name: 'Awesome Cotton Sausages',
+    imageURLs: ['https://loremflickr.com/640/480/technics'],
+    description:
+      'Consequuntur laudantium accusantium aperiam fugit pariatur neque. Necessitatibus aperiam provident possimus earum corporis incidunt quam quia ipsam. Dicta earum quos.',
+    stock: 18266,
+    currency: '$',
+    amount: 51927,
+    id: '4',
+  },
+  {
+    createdAt: '2024-01-02T04:36:37.029Z',
+    name: 'Tasty Metal Bacon',
+    imageURLs: ['https://loremflickr.com/640/480/technics'],
+    description:
+      'Vero pariatur alias ad repellendus animi deleniti sequi ratione. Voluptate rem velit sit error sapiente voluptatibus perferendis at. Necessitatibus doloremque accusantium dolorum veritatis explicabo impedit iusto. Neque consequuntur modi illo quo. Natus odio tempore occaecati similique fugiat enim ab veniam iusto.',
+    stock: 25251,
+    currency: '$',
+    amount: 52280,
+    id: '5',
+  },
+];
