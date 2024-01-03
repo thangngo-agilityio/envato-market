e43afbf2070d9b43abd7ddf72c974dc7ef07d2ab
Son Xuan Son <son.phan@asnet.com.vn>
2024-01-03 11:09:16 +0700

    feat: style hover after

--- landing-page/src/layouts/Main/SideBar/index.tsx
+++ landing-page/src/layouts/Main/SideBar/index.tsx
@@ -5,12 +5,31 @@ import CloseSideBar from '@app/components/icons/CloseSideBar/index.tsx';
 // Mocks
 import { NAVBAR } from '@app/mocks';
 
+// TODO: Update to late
+const pathName: string = '/';
+
+// style hover for list item
+const afterStyle: string = [
+  'absolute',
+  'z-10',
+  'top-[50%]',
+  'left-[-70px]',
+  'w-[30px]',
+  'h-[3px]',
+  'delay-500',
+  'duration-500',
+].join(' after:');
+
+console.log('after: ', afterStyle);
+
+// const transitionStyles: string = ['transition', 'duration-500'].join(' ');
+
 const SideBarAllDevices = (): JSX.Element => (
-  <header className='relative z-10 max-w-[320px] bg-white py-[60px] px-[70px] h-full'>
-    <Button className='bg-sun'>
-      <CloseSideBar width='12' height='12' />
+  <header className='relative z-10 max-w-[320px] bg-white py-[60px] px-[70px] h-full hidden md:block'>
+    <Button className='bg-sun w-10 h-10 flex justify-center items-center absolute top-0 right-md md:hidden'>
+      <CloseSideBar width='9' height='12' />
     </Button>
-    <h1>
+    <h1 className='mb-[100px]'>
       <a href='#'>
         <img
           src='/assets/logo-header-mobile.webp'
@@ -25,10 +44,12 @@ const SideBarAllDevices = (): JSX.Element => (
       <ul>
         {NAVBAR.map(
           ({ id, href, text }): JSX.Element => (
-            <li key={id} className='px-5'>
+            <li key={id}>
               <a
                 href={href}
-                className='text-secondary hover:text-sun uppercase text-sm'
+                className={`${
+                  href === pathName ? 'text-sun' : ''
+                } text-secondary hover:text-sun uppercase text-sm leading-[53px] py-5 relative w-min-content after:w-0 after:h-1 after:transition-all after:duration-500 hover:after:absolute hover:after:z-10 hover:after:top-[50%] hover:after:left-[-70px] hover:after:w-[30px] hover:after:h-[3px] hover:after:bg-sun`}
               >
                 {text}
               </a>
