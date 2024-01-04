6abf1997e8dfbb33456de68b059e7c3ec57ee6da
CanhVo <canh.vo@asnet.com.vn>
2024-01-04 16:42:17 +0700

    fix: update next config

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
-    reactStrictMode: true,
-    swcMinify: true,
     remotePatterns: [
       {
-        minimumCacheTTL: 60,
         protocol: 'https',
         hostname: '**',
       },
