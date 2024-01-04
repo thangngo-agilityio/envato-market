04cf2caf06864e08abe0dd287daa70d8a55df00e
duong.pham <duong.pham@asnet.com.vn>
2024-01-04 10:59:48 +0700

    feat: intergate API for home page

--- landing-page/src/interfaces/product.ts
+++ landing-page/src/interfaces/product.ts
@@ -4,7 +4,7 @@ export interface IProduct {
   description: string;
   category: string;
   currency: string;
-  imageURL: string[];
+  imageURLs: string[];
   amount: number;
   stock: number;
   createdAt: number;
