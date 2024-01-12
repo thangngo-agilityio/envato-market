b67f45ca96f0336aecb1e2e988abd886abe6b9d4
CanhVo <canh.vo@asnet.com.vn>
2024-01-12 17:49:17 +0700

    fix: fetch revenue data in server

--- dashboard/app/ui/components/TotalStatisticList/index.tsx
+++ dashboard/app/ui/components/TotalStatisticList/index.tsx
@@ -2,58 +2,38 @@ import { memo } from 'react';
 
 // Components
 import { Grid, GridItem } from '@chakra-ui/react';
-import {
-  TotalStatisticCard,
-  // TotalStatisticListSkeleton,
-} from '@/ui/components';
+import { TotalStatisticCard } from '@/ui/components';
 
 // Types
 import { ISpendingStatistics } from '@/lib/interfaces';
 
 // Mocks
 import { INITIAL_TOTAL_STATISTICS } from '@/lib/mocks';
-import { getStatistical } from '@/lib/services';
-import { END_POINTS } from '@/lib/constants';
-import { QueryProvider } from '@/ui/providers';
 
-// interface TotalListComponentProps {
-//   spendingStatistics?: ISpendingStatistics[];
-//   isLoading?: boolean;
-// }
+interface TotalStatisticListProps {
+  data?: ISpendingStatistics[];
+}
 
-const TotalStatisticListComponent = async () => {
-  // if (isLoading) return <TotalStatisticListSkeleton />;
-  const spendingStatistics =
-    (await getStatistical<ISpendingStatistics[]>(END_POINTS.STATISTICS)) ||
-    INITIAL_TOTAL_STATISTICS;
-
-  return (
-    <Grid
-      templateColumns={{ base: 'repeat(1, 1fr)', xl: 'repeat(3, 1fr)' }}
-      gap={6}
-    >
-      {spendingStatistics.map(
-        ({ title, total, growth, weeklyIncome }: ISpendingStatistics) => (
-          <GridItem key={title}>
-            <TotalStatisticCard
-              title={title}
-              total={total}
-              growth={growth}
-              weeklyIncome={weeklyIncome}
-            />
-          </GridItem>
-        ),
-      )}
-    </Grid>
-  );
-};
-
-const WrappedTotalStatisticList = () => (
-  <QueryProvider>
-    <TotalStatisticListComponent />
-  </QueryProvider>
+const TotalStatisticListComponent = ({
+  data = INITIAL_TOTAL_STATISTICS,
+}: TotalStatisticListProps) => (
+  <Grid
+    templateColumns={{ base: 'repeat(1, 1fr)', xl: 'repeat(3, 1fr)' }}
+    gap={6}
+  >
+    {data.map(({ title, total, growth, weeklyIncome }: ISpendingStatistics) => (
+      <GridItem key={title}>
+        <TotalStatisticCard
+          title={title}
+          total={total}
+          growth={growth}
+          weeklyIncome={weeklyIncome}
+        />
+      </GridItem>
+    ))}
+  </Grid>
 );
 
-const TotalStatisticList = memo(WrappedTotalStatisticList);
+const TotalStatisticList = memo(TotalStatisticListComponent);
 
 export default TotalStatisticList;
--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
         hostname: '**',
       },
     ],
+    formats: ['image/webp'],
   },
 };
 
