feb7a7a7517bb6cb3754a2089ba8379f9b5e2654
duong.pham <duong.pham@asnet.com.vn>
2024-01-15 10:27:45 +0700

    fix: optimize image

--- landing-page/src/components/index.ts
+++ landing-page/src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Heading } from './Heading/index.astro';
 export { default as Input } from './common/Input';
 export { default as InputNumber } from './common/Input/InputNumber';
+export { default as Image } from './common/Image';
 export { default as Container } from './common/Container/index.astro';
 export { default as Button } from './Button';
 export { default as ProductCard } from './ProductCard/index.astro';
