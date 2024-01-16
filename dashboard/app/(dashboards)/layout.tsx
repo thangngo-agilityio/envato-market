0cefcbbcb17759b9f106de6871479270c5c330d5
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2024-01-16 14:07:37 +0700

    feat: use props user instead of authStore

--- dashboard/app/(dashboards)/layout.tsx
+++ dashboard/app/(dashboards)/layout.tsx
@@ -1,33 +1,44 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 import { SIDEBAR } from '@/lib/constants';
 import { Box, Flex } from '@chakra-ui/react';
 
 // Component
 import { Header } from '@/ui/layouts';
+import { TAuthStoreData, authStore } from '@/lib/stores';
+
+// Interfaces
+import { TUserDetail } from '@/lib/interfaces';
+
 const SideBar = dynamic(() => import('@/ui/layouts/Sidebar'));
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => (
-  <Flex w="full" h="full" bg="background.body.primary">
-    <Box
-      pl={{
-        base: 0,
-        md: SIDEBAR.MINI_SIDEBAR_WIDTH,
-        lg: SIDEBAR.MINI_SIDEBAR_WIDTH,
-        '4xl': SIDEBAR.EXPAND_SIDEBAR_WIDTH,
-      }}
-      w="full"
-      minH="100vh"
-      h="full"
-      sx={{
-        transition: 'all .25s ease-in-out',
-      }}
-    >
-      <SideBar />
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const user = authStore((state): TAuthStoreData['user'] => state.user);
 
-      <Header />
-      {children}
-    </Box>
-  </Flex>
-);
+  return (
+    <Flex w="full" h="full" bg="background.body.primary">
+      <Box
+        pl={{
+          base: 0,
+          md: SIDEBAR.MINI_SIDEBAR_WIDTH,
+          lg: SIDEBAR.MINI_SIDEBAR_WIDTH,
+          '4xl': SIDEBAR.EXPAND_SIDEBAR_WIDTH,
+        }}
+        w="full"
+        minH="100vh"
+        h="full"
+        sx={{
+          transition: 'all .25s ease-in-out',
+        }}
+      >
+        <SideBar />
+
+        <Header user={user as TUserDetail} />
+        {children}
+      </Box>
+    </Flex>
+  );
+};
 
 export default MainLayout;
