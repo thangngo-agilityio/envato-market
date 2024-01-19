7bddd290030e1193eab8b5c4a8dd494f264b1458
duong.pham <duong.pham@asnet.com.vn>
2024-01-19 10:56:01 +0700

    fix: dismiss Hamburger menu when clicking outside

--- landing-page/src/layouts/Main/HeaderMobile/index.tsx
+++ landing-page/src/layouts/Main/HeaderMobile/index.tsx
@@ -4,36 +4,45 @@ import { HamburgerMenuIcon } from '@app/components/icons';
 
 // Constant
 import { ROUTES } from '@app/constants';
+import { useCallback, type MouseEventHandler } from 'react';
 
 type THeaderProps = {
   onToggleSidebar?: () => void;
 };
 
 const HeaderMobile = ({ onToggleSidebar }: THeaderProps): JSX.Element => {
-
   // Styles CSS
-  const styleHeader: string = 'bg-white flex justify-between items-center fixed z-20 top-0 left-0 right-0 h-[70px] shadow-navMobile py-2 px-8 md:hidden';
+  const styleHeader: string =
+    'bg-white flex justify-between items-center fixed z-20 top-0 left-0 right-0 h-[70px] shadow-navMobile py-2 px-8 md:hidden';
+
+  const handleOpenSidebar: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (e) => {
+      e.stopPropagation();
+
+      onToggleSidebar && onToggleSidebar();
+    },
+    [onToggleSidebar],
+  );
 
   return (
     <header className={`${styleHeader}`}>
-    <a href={ROUTES.HOME} className='text-infoRGBA'>
-      <img
-        src='/assets/logo-header-mobile.webp'
-        alt='Logo'
-        width={70}
-        height={28}
-        loading='lazy'
-      />
-    </a>
-    <Button
-      className='flex justify-center items-center bg-transparent hover:bg-transparent'
-      onClick={onToggleSidebar}
-      aria-label='Show Menu'
-    >
-      <HamburgerMenuIcon className='fill-sun stroke-2' />
-    </Button>
-  </header>
+      <a href={ROUTES.HOME} className='text-infoRGBA'>
+        <img
+          src='/assets/logo-header-mobile.webp'
+          alt='Logo'
+          width={70}
+          height={28}
+          loading='lazy'
+        />
+      </a>
+      <Button
+        className='flex justify-center items-center bg-transparent hover:bg-transparent'
+        onClick={handleOpenSidebar}
+        aria-label='Show Menu'
+      >
+        <HamburgerMenuIcon className='fill-sun stroke-2' />
+      </Button>
+    </header>
   );
-  
 };
 export default HeaderMobile;
