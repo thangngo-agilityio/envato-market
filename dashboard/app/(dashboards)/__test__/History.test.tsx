21b9a833b5da84839416dee5eacb42ab624d8d4c
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2024-01-28 21:45:26 +0700

    feat: update unit test for history page

--- dashboard/app/(dashboards)/__test__/History.test.tsx
+++ dashboard/app/(dashboards)/__test__/History.test.tsx
@@ -1,17 +1,13 @@
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+
 // Sections
-
-import { usePathname, useSearchParams } from 'next/navigation';
 import HistoryPage from '../histories/page';
-import { useRouter } from 'next/router';
-
-const { render } = testLibReactUtils;
+import { renderQueryProviderTest } from '@/lib/utils/testUtils';
 
 jest.mock('next/navigation', () => ({
   useSearchParams: jest.fn(),
   usePathname: jest.fn(),
-  useRouter: () => ({
-    push: jest.fn(),
-  }),
+  useRouter: jest.fn(),
 }));
 
 describe('HistoryPage render', () => {
@@ -22,12 +18,15 @@ describe('HistoryPage render', () => {
 
   (usePathname as jest.Mock).mockReturnValue(() => '');
 
-  (useRouter as jest.Mock).mockReturnValue(() => ({
+  (useRouter as jest.Mock).mockReturnValue({
+    pathname: '/test',
+    query: { name: 'testQuery' },
     push: jest.fn(),
-  }));
+    replace: jest.fn(),
+  });
 
   test('Should render match with snapshot.', () => {
-    const { container } = render(<HistoryPage />);
+    const { container } = renderQueryProviderTest(<HistoryPage />);
 
     expect(container).toMatchSnapshot();
   });
