bd6b36c115255911b6c69afbc294b18a64dd8928
Son Xuan Son <son.phan@asnet.com.vn>
2024-01-29 17:02:32 +0700

    feat: create a file storybook for message admin component

---
+++ dashboard/app/ui/components/MessagesAdmin/MessagesAdmin.stories.tsx
@@ -0,0 +1,21 @@
+import { StoryObj, Meta } from '@storybook/react';
+
+// Components
+import MessageAdmin from './index';
+
+const meta: Meta<typeof MessageAdmin> = {
+  title: 'Custom Components/MessageAdmin',
+  component: MessageAdmin,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof MessageAdmin>;
+
+export const MessageAdminCard: Story = {
+  args: {
+    content: 'This is a message with custom avatars',
+    isSuperAdmin: true,
+  },
+};
