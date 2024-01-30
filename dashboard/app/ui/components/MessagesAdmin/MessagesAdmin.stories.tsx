d10101b0599d81d27c06c0fb738637000569ac8d
Son Xuan Son <son.phan@asnet.com.vn>
2024-01-30 12:05:25 +0700

    feat: update prop in argTypes

--- dashboard/app/ui/components/MessagesAdmin/MessagesAdmin.stories.tsx
+++ dashboard/app/ui/components/MessagesAdmin/MessagesAdmin.stories.tsx
@@ -3,11 +3,30 @@ import { StoryObj, Meta } from '@storybook/react';
 // Components
 import MessageAdmin from './index';
 
+// Constants
+import { IMAGES } from '@/lib/constants';
+
 const meta: Meta<typeof MessageAdmin> = {
   title: 'Custom Components/MessageAdmin',
   component: MessageAdmin,
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    content: {
+      description: 'This is content message of supper admin or member',
+    },
+    avatarUser: {
+      src: IMAGES.CHAT_USER_AVATAR.url,
+    },
+    avatarAdmin: {
+      src: IMAGES.CHAT_USER_AVATAR.url,
+    },
+    localeTime: {
+      description: 'This is locale time message',
+    },
+    isSuperAdmin: {
+      expanded: true,
+    },
+  },
 };
 
 export default meta;
@@ -15,13 +34,17 @@ type Story = StoryObj<typeof MessageAdmin>;
 
 export const Default: Story = {
   args: {
-    content: 'This is a message with custom avatars',
+    content: 'This is content message of supper admin or member',
+    isSuperAdmin: false,
+    avatarUser: IMAGES.CHAT_USER_AVATAR.url,
+    avatarAdmin: IMAGES.CHAT_USER_AVATAR.url,
+    localeTime: 'This is locale time message',
   },
 };
 
 export const MessageAdminCard: Story = {
   args: {
-    content: 'This is a message with custom avatars',
+    content: 'This is content message of supper admin or member',
     isSuperAdmin: true,
   },
 };
