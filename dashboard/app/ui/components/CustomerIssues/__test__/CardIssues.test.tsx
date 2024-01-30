61a0da172f70498b5fe324020f56c76ecd18c490
Loc Thanh Vo <loc.vo@asnet.com.vn>
2024-01-30 17:14:02 +0700

    feat: write unit testing for CustomerIssues, CardIssues component

--- dashboard/app/ui/components/CustomerIssues/__test__/CardIssues.test.tsx
+++ dashboard/app/ui/components/CustomerIssues/__test__/CardIssues.test.tsx
@@ -7,8 +7,16 @@ import { CardIssues } from '@/ui/components';
 import { ISSUES } from '@/lib/mocks';
 
 describe('CardIssues render', () => {
-  test('Should render match with snapshot.', () => {
+  it('Should render match with snapshot.', () => {
     const { container } = render(<CardIssues data={ISSUES} />);
     expect(container).toMatchSnapshot();
+  });
+
+  it('displays user information correctly', () => {
+    const { getByText } = render(<CardIssues data={ISSUES} />);
+
+    expect(
+      getByText(`${ISSUES.firstName} ${ISSUES.lastName}`),
+    ).toBeInTheDocument();
   });
 });
