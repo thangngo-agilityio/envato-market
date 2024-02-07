7a8ebd7411fd9e208a4e20d24906d8ced11e2cba
thangngo <thangngo@asnet.com.vn>
2024-02-07 09:58:25 +0700

    feat: check query provider for totalBalance

--- dashboard/app/ui/components/TotalBalance/TotalBalance.stories.tsx
+++ dashboard/app/ui/components/TotalBalance/TotalBalance.stories.tsx
@@ -1,12 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StoryObj, Meta } from '@storybook/react';
 
 // Components
 import TotalBalance from '@/ui/components/TotalBalance';
 
+const queryClient = new QueryClient();
+
 const meta: Meta<typeof TotalBalance> = {
   title: 'Custom Components/TotalBalance',
   tags: ['autodocs'],
   component: TotalBalance,
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
   parameters: {
     controls: {
       expanded: true,
