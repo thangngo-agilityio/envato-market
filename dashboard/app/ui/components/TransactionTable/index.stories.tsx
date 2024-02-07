fa9dc05212b06b44a2f7764f4caa56811a84045f
thangngo <thangngo@asnet.com.vn>
2024-02-07 09:41:03 +0700

    feat: add query client for filter user

--- dashboard/app/ui/components/TransactionTable/index.stories.tsx
+++ dashboard/app/ui/components/TransactionTable/index.stories.tsx
@@ -3,6 +3,9 @@ import { Box } from '@chakra-ui/react';
 
 // Component
 import FilterUser from '@/ui/components/TransactionTable';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const meta: Meta<typeof FilterUser> = {
   title: 'Custom Components/FilterUser',
@@ -10,14 +13,16 @@ const meta: Meta<typeof FilterUser> = {
   component: FilterUser,
   decorators: [
     (Story) => (
-      <Box
-        bgColor="background.component.primary"
-        borderRadius={8}
-        px={6}
-        py={5}
-      >
-        <Story />
-      </Box>
+      <QueryClientProvider client={queryClient}>
+        <Box
+          bgColor="background.component.primary"
+          borderRadius={8}
+          px={6}
+          py={5}
+        >
+          <Story />
+        </Box>
+      </QueryClientProvider>
     ),
   ],
   parameters: {
