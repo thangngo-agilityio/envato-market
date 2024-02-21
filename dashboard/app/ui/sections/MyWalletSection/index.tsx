334f91e7bb535317adc216004f5266dbc2ef21d2
CanhVo <canh.vo@asnet.com.vn>
2024-02-21 15:03:29 +0700

    fix: avoid layout shift for mywallet

--- dashboard/app/ui/sections/MyWalletSection/index.tsx
+++ dashboard/app/ui/sections/MyWalletSection/index.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 
 //Components
-import { Box, Flex, Grid, GridItem, useMediaQuery } from '@chakra-ui/react';
+import { Box, Flex, useMediaQuery } from '@chakra-ui/react';
 
 import { useEffect, useState } from 'react';
 
@@ -11,9 +11,7 @@ import { useEffect, useState } from 'react';
 const TransactionTable = dynamic(
   () => import('@/ui/components/TransactionTable'),
 );
-const CardPayment = dynamic(() => import('@/ui/components/CardPayment'));
 const Efficiency = dynamic(() => import('@/ui/components/Efficiency'));
-const TotalBalance = dynamic(() => import('@/ui/components/TotalBalance'));
 const OverallBalance = dynamic(() => import('@/ui/components/OverallBalance'));
 
 const MyWalletsSection = () => {
@@ -27,54 +25,36 @@ const MyWalletsSection = () => {
     });
   }, [isNotMobile]);
 
-  console.log(showBelow);
-
   return (
-    <Grid
-      bg="background.body.primary"
-      px={{ base: 6, md: 12 }}
-      py={12}
-      templateColumns={{ base: 'repeat(1, 1fr)', '3xl': 'repeat(4, 1fr)' }}
-      gap={{ base: 0, '2xl': 6 }}
-      display={{ sm: 'block', xl: 'grid' }}
-      minH="100vh"
-    >
-      <GridItem colSpan={1}>
-        <Flex w="full" direction="column" gap={6}>
-          <TotalBalance />
-          <CardPayment />
-        </Flex>
-      </GridItem>
-      <GridItem colSpan={{ base: 1, xl: 3 }} mt={{ base: 6, '3xl': 0 }}>
-        <Flex direction="column" gap={6}>
-          <Flex
-            flex={1}
-            gap={6}
-            direction={{ base: 'column', xl: 'row' }}
-            boxSizing="border-box"
-            w="100%"
-          >
-            <Box w={{ '3xl': '65%' }} flex={2}>
-              {showBelow && <OverallBalance />}
-            </Box>
-            <Box w={{ '3xl': '35%' }} flex={1}>
-              {showBelow && <Efficiency />}
-            </Box>
-          </Flex>
-          <Box>
-            <Box
-              as="section"
-              bgColor="background.component.primary"
-              borderRadius={8}
-              px={6}
-              py={5}
-            >
-              {showBelow && <TransactionTable />}
-            </Box>
+    showBelow && (
+      <Flex direction="column" gap={6}>
+        <Flex
+          flex={1}
+          gap={6}
+          direction={{ base: 'column', xl: 'row' }}
+          boxSizing="border-box"
+          w="100%"
+        >
+          <Box w={{ '3xl': '65%' }} flex={2}>
+            <OverallBalance />
+          </Box>
+          <Box w={{ '3xl': '35%' }} flex={1}>
+            <Efficiency />
           </Box>
         </Flex>
-      </GridItem>
-    </Grid>
+        <Box>
+          <Box
+            as="section"
+            bgColor="background.component.primary"
+            borderRadius={8}
+            px={6}
+            py={5}
+          >
+            <TransactionTable />
+          </Box>
+        </Box>
+      </Flex>
+    )
   );
 };
 
