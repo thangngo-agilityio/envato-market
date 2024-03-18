b30e0bc40222f7a59abc3608765af32dcc448b3a
Son Xuan Son <son.phan@asnet.com.vn>
2024-03-18 11:43:41 +0700

    feat: rename for icon sun and moon component

--- landing-page/src/components/SwitchTheme/index.tsx
+++ landing-page/src/components/SwitchTheme/index.tsx
@@ -5,8 +5,8 @@ import { useContext, useMemo, useCallback } from 'react';
 import { THEMES } from '../../../../dashboard/app/lib/constants/themes';
 
 // Components
-import DarkIcon from '@app/components/icons/Dark/index.tsx';
-import LightIcon from '@app/components/icons/Light/index.tsx';
+import SunIcon from '@app/components/icons/Sun';
+import MoonIcon from '@app/components/icons/Moon';
 import Button from '@app/components/Button/index';
 import { ThemeContext } from '@app/context/ThemeContext';
 
@@ -17,7 +17,7 @@ const SwitchTheme = () => {
   const isDarkTheme = useMemo(() => theme === THEMES.DARK, [theme]);
 
   // UseMemo is used to prevent unnecessary re-renders
-  const icon = useMemo(() => !isDarkTheme ? <DarkIcon /> : <LightIcon />, [isDarkTheme]);
+  const icon = useMemo(() => !isDarkTheme ? <SunIcon /> : <MoonIcon />, [isDarkTheme]);
 
   // Handle theme change when clicked 
   const handleChange = useCallback(() => setTheme(isDarkTheme ? THEMES.LIGHT : THEMES.DARK), [setTheme, isDarkTheme]);
