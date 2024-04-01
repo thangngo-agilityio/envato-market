8cebef407c0f740005591bb263b8cc68198174fd
Son Xuan Son <son.phan@asnet.com.vn>
2024-04-01 17:21:15 +0700

    fix: update label for link from .env

--- landing-page/src/mocks/linkAPI.ts
+++
@@ -1,5 +0,0 @@
-export const LINK_API = {
-  API_PRODUCTS: 'https://659394231493b0116068ba18.mockapi.io',
-  PUBLIC_API_PRODUCTS: 'https://659394231493b0116068ba18.mockapi.io',
-  PUBLIC_API_CHECKOUT: 'https://succinct-shrouded-acorn.glitch.me',
-};
