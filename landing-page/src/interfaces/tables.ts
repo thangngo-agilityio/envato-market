9d4717b702e1a0fc34b02e8b3be0420e2da41eab
Son Xuan Son <son.phan@asnet.com.vn>
2024-04-04 09:09:14 +0700

    feat: refactor code style table

---
+++ landing-page/src/interfaces/tables.ts
@@ -0,0 +1,12 @@
+export interface ITABLE {
+  NameEntity?: string;
+  Address?: string;
+  ScopeCooperation?: string;
+}
+
+export interface ITABLES_COUNT {
+  NameEntity?: string;
+  Address?: string;
+  Country?: string;
+  ScopeCooperation?: string;
+}
