19f1054fbf4e3ae3c9a40ebc893a5edea299ced7
Thang Huu Quoc Ngo <thang.ngo@asnet.com.vn>
2024-04-09 16:25:33 +0700

    fix: write unit test for recent activities section

---
+++ dashboard/app/ui/sections/__test__/RecentActivitiesSection.test.tsx
@@ -0,0 +1,30 @@
+import preloadAll from 'jest-next-dynamic';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+// Utils
+import { renderQueryProviderTest } from '@/lib/utils/testUtils';
+
+// Sections
+import RecentActivities from '../RecentActivitiesSection';
+
+describe('RecentActivitiesSection render', () => {
+  beforeEach(async () => {
+    await preloadAll();
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      push: jest.fn(),
+    }));
+    (useSearchParams as jest.Mock).mockImplementation(
+      () => new URLSearchParams(),
+    );
+
+    (useSearchParams as jest.Mock).mockReturnValue({
+      get: jest.fn(),
+    });
+  });
+
+  test('Should render match with snapshot.', () => {
+    const { container } = renderQueryProviderTest(<RecentActivities />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
