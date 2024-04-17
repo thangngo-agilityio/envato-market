df8995a5fa406b83460a6a44d38aaf0984eeec35
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-04-17 14:19:03 +0700

    fix: update config to remove console on production

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -3,16 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   images: {
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: '**',
-    }, ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
     formats: ['image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   compiler: {
-    // removeConsole: true,
+    removeConsole: process.env.NODE_ENV === 'production',
   },
 };
 
