9918aa87e5a91a2d5d35e972468ceecb1bd5c716
Ngoc Ngo Thi <ngoc.ngo@asnet.com.vn>
2024-04-24 08:53:09 +0700

    [EM-1136] Config for cache images

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  minimumCacheTTL: 60 * 30,
   reactStrictMode: true,
   swcMinify: false,
   images: {
@@ -9,12 +10,27 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-    formats: ['image/webp'],
+    formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png |webp)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+    ];
   },
 };
 
